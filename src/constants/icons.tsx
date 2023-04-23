46227ad1a2a5300161a42d5572764362e3771db8
NAbin RAi <nabinr@gmeremit.com>
2023-04-23 19:56:46 +0545

    rearrange folder

---
+++ src/constants/icons.tsx
@@ -0,0 +1,13 @@
+import search from '../../assets/icons/search.png'
+import home from '../../assets/icons/home.png'
+import cart from '../../assets/icons/cart.png'
+import profile from '../../assets/icons/profile.png'
+import heart from '../../assets/icons/heart.png'
+
+export default{
+    search,
+    home,
+    cart,
+    profile,
+    heart
+};
